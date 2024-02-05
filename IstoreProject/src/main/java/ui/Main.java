8ca09513d0d685b373722e9705691450b64c37b5
rony140G <glenronel@gmail.com>
2024-02-05 20:48:24 +0100

    projet initale

--- IstoreProject/src/main/java/ui/Main.java
+++ IstoreProject/src/main/java/ui/Main.java
@@ -7,12 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println( "*********************iStore*******************test" );
+        System.out.println( "*********************iStore*******************t" );
         new LoginUI().show();
 
     }
-
-
-
 }
 
