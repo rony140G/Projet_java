511b0c8aa346e92ccc3edad5f2f203466c5655ca
Flapacha22 <hugomouba49@gmail.com>
2024-02-20 20:18:57 +0100

    Code presque complet

--- IstoreProject/src/main/java/infrastructure/ManageStoreDao.java
+++ IstoreProject/src/main/java/infrastructure/ManageStoreDao.java
@@ -59,16 +59,118 @@ public class ManageStoreDao {
         }
     }
 
+    public static void DelArticle(String nomArticle, int nbrArticle, int inventoryID) {
+        String sql = "UPDATE article SET stock_disponible = stock_disponible - ? WHERE Nom_article = ? AND Inventory_ID = ?";
+        try (Connection connection = DatabaseCo.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, nbrArticle);
+            statement.setString(2, nomArticle);
+            statement.setInt(3, inventoryID);
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Number of articles deleted successfully.");
+            } else {
+                System.out.println("Failed to add articles.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error updating articles: " + e.getMessage());
+        }
+    }
+
     public void ViewStore() {
         String sql = "SELECT * FROM store";
         try (Connection connection = DatabaseCo.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                System.out.println( "Id : " + resultSet.getString("store_ID") +" ;Nom de la boutique : " + resultSet.getString("store_Name"));
+                System.out.println("Id : " + resultSet.getString("store_ID") + " ;Nom de la boutique : " + resultSet.getString("store_Name"));
             }
         } catch (SQLException e) {
             System.out.println("Error retrieving store: " + e.getMessage());
         }
     }
+
+    public static boolean InsertArticle(String article,int price, int stock_dispo, int ivent_id) {
+        String sql = "INSERT INTO Article (Nom_article, Prix, stock_disponible, Inventory_ID) VALUES (?, ?, ?, ?)";
+        try (Connection connexion = DatabaseCo.getConnection();
+             PreparedStatement statement = connexion.prepareStatement(sql)) {
+
+            statement.setString(1, article);
+            statement.setInt(2, price);
+            statement.setInt(3, stock_dispo);
+            statement.setInt(4, ivent_id);
+
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            System.out.println("Error inserting user: " + e.getMessage());
+            return false;
+        }
+
+    }
+
+    public static void ViewArticleInInventory(String storeName) {
+        String sql = "SELECT * FROM Article " +
+                "JOIN Inventory ON Article.Inventory_ID = Inventory.Inventory_ID " +
+                "JOIN Store ON Inventory.Store_ID = Store.Store_ID " +
+                "WHERE Store.Store_Name = ?";
+        try (Connection connection = DatabaseCo.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, storeName);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("Article ID: " + resultSet.getString("article_ID") +
+                        "; Nom de l'article: " + resultSet.getString("Nom_article") +
+                        "; Prix : " + resultSet.getString("Prix") +
+                        "; Stock disponible : " + resultSet.getString("stock_disponible"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving Inventory: " + e.getMessage());
+        }
+    }
+
+    public void manageinventory() {
+    }
+
+    public int getInventoryId(String storeName) {
+        String sql = "SELECT Inventory_ID FROM inventory " +
+                "JOIN store ON inventory.Store_ID = store.Store_ID " +
+                "WHERE store.Store_Name = ?";
+
+        try (Connection connection = DatabaseCo.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, storeName);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt("Inventory_ID");
+                } else {
+                    System.out.println("Inventory not found for store: " + storeName);
+                    return -1; // ou un autre valeur pour indiquer une absence d'inventaire
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving inventory ID: " + e.getMessage());
+            return -1;
+        }
+    }
+
+
+    public static void addArticle(String nomArticle, int nbrArticle, int inventoryID) {
+        String sql = "UPDATE article SET stock_disponible = stock_disponible + ? WHERE Nom_article = ? AND Inventory_ID = ?";
+        try (Connection connection = DatabaseCo.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, nbrArticle);
+            statement.setString(2, nomArticle);
+            statement.setInt(3, inventoryID);
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Number of articles added successfully.");
+            } else {
+                System.out.println("Failed to add articles.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error updating articles: " + e.getMessage());
+        }
+    }
+
 }
--- IstoreProject/src/main/java/ui/ManageStoreUi.java
+++ IstoreProject/src/main/java/ui/ManageStoreUi.java
@@ -3,6 +3,7 @@ package ui;
 import infrastructure.ManageStoreDao;
 
 
+
 import java.util.*;
 
 import static infrastructure.ManageStoreDao.*;
@@ -35,9 +36,12 @@ public class ManageStoreUi {
                 case 3:
                     DeleteStoreUi();
                     break;
+                case 4:
+                    InventoryMenu();
                 default:
                     System.out.println("Invalid option. Please try again.");
                     break;
+
             }
         }
     }
@@ -62,6 +66,99 @@ public class ManageStoreUi {
         }else {
             System.out.println("store don't created !");
         }
+    }
+
+    public void InventoryMenu (){
+        boolean bunning = true;
+        while (bunning) {
+            System.out.println("********************* Menu Inventory *******************");
+            System.out.println("Back to manage store, press 0");
+            System.out.println("to Create article, press 1 ");
+            System.out.println("to view  article, press 2");
+            System.out.println("to add article in inventory , press 3");
+            System.out.println("to withdrawn article in inventory , press 4");
+            int choice = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    bunning = false;
+                    break;
+                case 1:
+                    CreateArticle();
+                    break;
+                case 2:
+                    ViewArticle();
+                    break;
+                case 3:
+                    AddToStock();
+                    break;
+                case 4:
+                    DeletteToStock();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+
+            }
+        }
+        new ManageStoreDao().manageinventory();
+    }
+
+    private void DeletteToStock() {
+        System.out.println("Dans quelle boutique voulez vous supprimer votre article");
+        String Boutique = scanner.nextLine();
+        int ID_s = new ManageStoreDao().getInventoryId(Boutique);
+
+        System.out.println("Nom de l' article");
+        String NomArticle = scanner.nextLine();
+
+        System.out.println("Nombre à supprimeé");
+        int NombreArticle = scanner.nextInt();
+
+        ManageStoreDao.DelArticle(NomArticle, NombreArticle,ID_s);
+    }
+
+    private void ViewArticle() {
+        System.out.println("Entrez le nom de la boutique dont vous voulez voire l'inventaire :");
+        String Boutique = scanner.nextLine();
+
+        ManageStoreDao.ViewArticleInInventory(Boutique);
+    }
+
+    private void CreateArticle() {
+        System.out.println("Dans quelle boutique souhaiter vous ajouter un article ?");
+        String store = scanner.nextLine();
+        int ID_Store = new ManageStoreDao().getInventoryId(store);
+
+        System.out.println("Entrez le nom de l'article ");
+        String Article = scanner.nextLine();
+
+        System.out.println("Entrez le prix de l'article");
+        int price =scanner.nextInt();
+
+        System.out.println("Stock ?");
+        int stock = scanner.nextInt();
+
+        if(ManageStoreDao.InsertArticle(Article, price, stock, ID_Store)){
+            System.out.println("Article ajouté avec succés");
+        }
+    }
+
+
+    public void AddToStock(){
+        System.out.println("Dans quelle boutique voulez vous ajouter votre article");
+        String Boutique = scanner.nextLine();
+        int ID_s = new ManageStoreDao().getInventoryId(Boutique);
+
+        System.out.println("Nom de l' article");
+        String NomArticle = scanner.nextLine();
+
+        System.out.println("Nombre à ajouter");
+        int NombreArticle = scanner.nextInt();
+
+        ManageStoreDao.addArticle(NomArticle, NombreArticle,ID_s);
     }
     public static void main(String[] args) {
         ManageStoreUi manageStoreUi = new ManageStoreUi();
